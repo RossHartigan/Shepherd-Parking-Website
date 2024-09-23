dd8ef11a983b4923dd3a342540c3ae2c55d2c86b
Brandon Pye - ST10238400 <104008944+MrDarcy314159@users.noreply.github.com>
2024-09-23 14:15:08 +0200

    Firebase Container

--- Shepherd-Parking-Website/obj/Debug/net8.0/Shepherd-Parking-Website.AssemblyInfo.cs
+++ Shepherd-Parking-Website/obj/Debug/net8.0/Shepherd-Parking-Website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4734bca2d2eea735c9cb4e4e485e157a4f611bef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6672026d9228f15c08e513d0eb93d75427644b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
