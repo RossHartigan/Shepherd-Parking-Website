10060b989200ee7e47bf1b23bda090a933b29208
RossHartigan <168533356+RossHartigan@users.noreply.github.com>
2024-09-23 12:15:26 +0200

    Website Setup

--- Shepherd-Parking-Website/obj/Release/net8.0/Shepherd-Parking-Website.AssemblyInfo.cs
+++ Shepherd-Parking-Website/obj/Release/net8.0/Shepherd-Parking-Website.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shepherd-Parking-Website")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7bf11bc1251155e06a9dc096612515d6ab2a0de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shepherd-Parking-Website")]
