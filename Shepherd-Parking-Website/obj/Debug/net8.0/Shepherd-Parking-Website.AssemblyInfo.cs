4d8e7b87594aa6ffd35da93a37dac689e8c6d7a7
RossHartigan <168533356+RossHartigan@users.noreply.github.com>
2024-09-23 14:51:30 +0200

    fixed maybe

--- Shepherd-Parking-Website/obj/Debug/net8.0/Shepherd-Parking-Website.AssemblyInfo.cs
+++ Shepherd-Parking-Website/obj/Debug/net8.0/Shepherd-Parking-Website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a62446583aef7ba22889a913a72fcb43a49aad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dcaf3d76fcba85ec915d82eb5ba999fb7e2e92c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
