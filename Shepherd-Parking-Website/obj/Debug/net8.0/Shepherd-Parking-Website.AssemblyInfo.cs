1d73a0964618b4cd86a0ccfcd83748fbd00f0824
RossHartigan <168533356+RossHartigan@users.noreply.github.com>
2024-09-25 15:27:17 +0200

    small touches

--- Shepherd-Parking-Website/obj/Debug/net8.0/Shepherd-Parking-Website.AssemblyInfo.cs
+++ Shepherd-Parking-Website/obj/Debug/net8.0/Shepherd-Parking-Website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f96e24546bc31957e0aa77a361b800af11bff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70614b1fa28185fa598989803122a62381dad722")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
