1633f022e544673082c3d0acb6f7d8ea9a5e3d19
Brandon Pye - ST10238400 <104008944+MrDarcy314159@users.noreply.github.com>
2024-09-26 12:23:54 +0200

    Done, I guess

---
+++ Shepherd-Parking-Website/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shepherd_Parking_Website.Pages
+{
+    public class AboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
